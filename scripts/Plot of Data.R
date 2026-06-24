04cd4bb3a07f1288e9111ee7845554dd29b0f88f
longachanda <longachandadoc88@gmail.com>
2026-06-24 12:11:26 +0200

    R code for plotting the Ebola data

---
+++ scripts/Plot of Data.R
@@ -0,0 +1,72 @@
+
+# Load Library
+library(readr)
+library(ggplot2)
+
+# Import Data set
+
+dta <- read_csv("C:/Users/ZNPHI/OneDrive/PHD/MMED/Github/drc_sitrep.csv")
+
+# Plot by data type
+
+dta_plot <- ggplot(dta, aes(x = date)) +
+  
+
+  # Suspect cases
+  geom_line(
+    aes(y = suspect_cases, colour = "Suspect Cases"),
+    linewidth = 1.1,
+    na.rm = TRUE
+  ) +
+  
+  # Suspect death
+  geom_line(
+    aes(y = suspect_death, colour = "Suspect Deaths"),
+    linewidth = 1.1,
+    na.rm = TRUE
+  ) +
+  
+  # Confirmed Cases
+  geom_line(
+    aes(y = confirmed_cases, colour = "Confirmed Cases"),
+    linewidth = 1.1,
+    linetype = "dashed",
+    na.rm = TRUE
+  ) +
+  
+  # Confirmed Deaths
+  geom_line(
+    aes(y = confirmed_death, colour = "Confirmed Deaths"),
+    linewidth = 1.1,
+    linetype = "dashed",
+    na.rm = TRUE
+  ) +
+  
+  scale_colour_manual(
+    values = c(
+      "Suspect Cases" = "blue",
+      "Suspect Deaths" = "darkblue",
+      "Confirmed Cases" = "pink",
+      "Confirmed Deaths" = "red"
+    )
+  ) +
+  
+  labs(
+    title = "Plot of Suspected and Confirmed Ebola Deaths and Cases",
+    subtitle = "",
+    x = "Date",
+    y = "Number of cases and Deaths",
+    colour = NULL
+  ) +
+  
+  theme_minimal(base_size = 13) +
+  theme(
+    plot.title = element_text(face = "bold"),
+    axis.title.y = element_text(colour = "steelblue"),
+    axis.title.y.right = element_text(colour = "darkred"),
+    legend.position = "bottom"
+  )
+
+
+#
+dta_plot
